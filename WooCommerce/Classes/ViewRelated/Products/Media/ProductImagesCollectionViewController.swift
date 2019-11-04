a622b3b6782846248db6aa54a5a281e07b8b2f90
Jaclyn Chen <watertranquil@gmail.com>
2019-11-04 10:59:19 +0800

    Clean up `ProductImagesCollectionViewController`.

--- WooCommerce/Classes/ViewRelated/Products/Media/ProductImagesCollectionViewController.swift
+++ WooCommerce/Classes/ViewRelated/Products/Media/ProductImagesCollectionViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Yosemite
 
-class ColumnFlowLayout: UICollectionViewFlowLayout {
+final class ColumnFlowLayout: UICollectionViewFlowLayout {
 
     let cellsPerRow: Int
 
@@ -35,16 +35,10 @@ class ColumnFlowLayout: UICollectionViewFlowLayout {
 
 }
 
-private let reuseIdentifier = "Cell"
-
-class ProductImagesCollectionViewController: UICollectionViewController {
+final class ProductImagesCollectionViewController: UICollectionViewController {
 
     private var productImages: [ProductImage]
-
-    private let sectionInsets = UIEdgeInsets(top: 50.0,
-                                             left: 20.0,
-                                             bottom: 50.0,
-                                             right: 20.0)
+    private let reuseIdentifier = "Cell"
 
     init(images: [ProductImage]) {
         self.productImages = images
@@ -66,14 +60,8 @@ class ProductImagesCollectionViewController: UICollectionViewController {
 
         collectionView.backgroundColor = StyleManager.wooWhite
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-//        collectionView.register(ProductImageCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         collectionView.register(ProductImageCollectionViewCell.loadNib(), forCellWithReuseIdentifier: reuseIdentifier)
 
-        // Do any additional setup after loading the view.
         collectionView.reloadData()
     }
 
@@ -82,18 +70,11 @@ class ProductImagesCollectionViewController: UICollectionViewController {
 
         collectionView.reloadData()
     }
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
+// MARK: UICollectionViewDataSource
+//
+extension ProductImagesCollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -110,39 +91,10 @@ class ProductImagesCollectionViewController: UICollectionViewController {
 
         let productImage = productImages[indexPath.row]
         cell.update(imageURL: productImage.src)
-    
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-    
 }
+
+// MARK: UICollectionViewDelegate
+//
+// TODO: when a Product image is selected
