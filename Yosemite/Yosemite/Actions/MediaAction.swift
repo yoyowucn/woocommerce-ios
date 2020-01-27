f3f096662b444e9a71b13a605361a3ec579b9902
Jaclyn Chen <watertranquil@gmail.com>
2020-01-27 12:18:20 +0800

    Polish `MediaStore`/`MediaAction`.

--- Yosemite/Yosemite/Actions/MediaAction.swift
+++ Yosemite/Yosemite/Actions/MediaAction.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-// MARK: - MediaAction: Defines stats operations (supported by the MediaStore).
+// MARK: - MediaAction: Defines media operations (supported by the MediaStore).
 //
 public enum MediaAction: Action {
 
@@ -8,5 +8,7 @@ public enum MediaAction: Action {
     ///
     case retrieveMediaLibrary(siteID: Int64, onCompletion: (_ mediaItems: [Media], _ error: Error?) -> Void)
 
+    /// Uploads an exportable media asset to the site's WP Media Library.
+    ///
     case uploadMedia(siteID: Int64, mediaAsset: ExportableAsset, onCompletion: (_ uploadedMedia: Media?, _ error: Error?) -> Void)
 }
--- Yosemite/Yosemite/Stores/MediaStore.swift
+++ Yosemite/Yosemite/Stores/MediaStore.swift
@@ -51,27 +51,35 @@ private extension MediaStore {
         }
     }
 
-    func uploadMedia(siteID: Int64, mediaAsset: ExportableAsset, onCompletion: @escaping (_ uploadedMedia: Media?, _ error: Error?) -> Void) {
+    /// Uploads an exportable media asset to the site's WP Media Library with 2 steps:
+    /// 1) Exports the media asset to a uploadable type
+    /// 2) Uploads the exported media file to the server
+    ///
+    func uploadMedia(siteID: Int64,
+                     mediaAsset: ExportableAsset,
+                     onCompletion: @escaping (_ uploadedMedia: Media?, _ error: Error?) -> Void) {
         let mediaExporter = MediaExportService()
-        _ = mediaExporter.export(mediaAsset,
-                                 onCompletion: { [weak self] (uploadableMedia, error) in
-                                    guard let uploadableMedia = uploadableMedia, error == nil else {
-                                        onCompletion(nil, error)
-                                        return
-                                    }
-                                    self?.uploadMedia(siteID: siteID,
-                                                      media: uploadableMedia,
-                                                      onCompletion: { (uploadedMedia, error) in
-                                                        onCompletion(uploadedMedia, error)
-                                    })
+        mediaExporter.export(mediaAsset,
+                             onCompletion: { [weak self] (uploadableMedia, error) in
+                                guard let uploadableMedia = uploadableMedia, error == nil else {
+                                    onCompletion(nil, error)
+                                    return
+                                }
+                                self?.uploadMedia(siteID: siteID,
+                                                  uploadableMedia: uploadableMedia,
+                                                  onCompletion: { (uploadedMedia, error) in
+                                                    onCompletion(uploadedMedia, error)
+                                })
         })
     }
 
-    func uploadMedia(siteID: Int64, media: UploadableMedia, onCompletion: @escaping (_ uploadedMedia: Media?, _ error: Error?) -> Void) {
+    func uploadMedia(siteID: Int64,
+                     uploadableMedia media: UploadableMedia,
+                     onCompletion: @escaping (_ uploadedMedia: Media?, _ error: Error?) -> Void) {
         let remote = MediaRemote(network: network)
         remote.uploadMedia(for: siteID,
                            mediaItems: [media]) { (uploadedMediaItems, error) in
-                            // Removes local media after the upload request.
+                            // Removes local media after the upload API request.
                             do {
                                 try MediaFileManager().removeLocalMedia(at: media.localURL)
                             } catch(let error) {
