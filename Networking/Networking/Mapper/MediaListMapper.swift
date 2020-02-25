89ae01590c669d8f030b37126e1ca8266516eb65
Jaclyn Chen <watertranquil@gmail.com>
2020-02-25 16:53:56 +0800

    Remove `nextPageHandle` from meta field.

--- Networking/Networking/Mapper/MediaListMapper.swift
+++ Networking/Networking/Mapper/MediaListMapper.swift
@@ -11,8 +11,7 @@ struct MediaListMapper: Mapper {
 }
 
 struct MediaListEnvelopeMapper: Mapper {
-
-    /// (Attempts) to convert a dictionary into an Account entity.
+    /// (Attempts) to convert a dictionary into a Media list.
     ///
     func map(response: Data) throws -> MediaListEnvelope {
         let decoder = JSONDecoder()
@@ -21,24 +20,14 @@ struct MediaListEnvelopeMapper: Mapper {
     }
 }
 
-struct MediaListMeta: Decodable {
-    let nextPageHandle: String
-
-    private enum CodingKeys: String, CodingKey {
-        case nextPageHandle = "next_page"
-    }
-}
-
-/// ProductEnvelope Disposable Entity:
-/// `Load All Products` endpoint returns the updated products document in the `data` key.
+/// MediaListEnvelope Disposable Entity:
+/// `Retrieve Media` endpoint returns the updated products document in the `media` key.
 /// This entity allows us to do parse all the things with JSONDecoder.
 ///
 struct MediaListEnvelope: Decodable {
     let mediaList: [Media]
-    let meta: MediaListMeta?
 
     private enum CodingKeys: String, CodingKey {
         case mediaList = "media"
-        case meta
     }
 }
