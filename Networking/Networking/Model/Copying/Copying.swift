debcfb541b63ec7520e41c77d4e49966f9272823
Shiki <jayson@basanes.net>
2020-04-19 09:00:43 -0600

    Add manual .copy and .nullify implementations

--- Networking/Networking/Model/Copying/Copying.swift
+++ Networking/Networking/Model/Copying/Copying.swift
@@ -14,10 +14,46 @@ public typealias Copiable<Wrapped> = Optional<Wrapped>
 
 public typealias NullableCopiable<Wrapped> = Optional<Optional<Wrapped>>
 
+// MARK: - Copiable.copy and NullableCopiable.copy alias
+
 extension Copiable where Wrapped == Codable {
     public static let copy: Wrapped? = nil
 }
 
+extension Copiable where Wrapped == String {
+    public static let copy: Wrapped? = nil
+}
+
+extension Copiable where Wrapped == Int {
+    public static let copy: Wrapped? = nil
+}
+
+extension Copiable where Wrapped == Int64 {
+    public static let copy: Wrapped? = nil
+}
+
+extension Copiable where Wrapped == Date {
+    public static let copy: Wrapped? = nil
+}
+
+// MARK: - NullableCopiable.nullify alias
+
 extension NullableCopiable where Wrapped == Codable? {
+    public static let nullify = Self.some(nil)
+}
+
+extension NullableCopiable where Wrapped == String? {
+    public static let nullify = Self.some(nil)
+}
+
+extension NullableCopiable where Wrapped == Int? {
+    public static let nullify = Self.some(nil)
+}
+
+extension NullableCopiable where Wrapped == Int64? {
+    public static let nullify = Self.some(nil)
+}
+
+extension NullableCopiable where Wrapped == Date? {
     public static let nullify = Self.some(nil)
 }
